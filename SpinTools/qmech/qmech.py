947ceafe34368b1625480d9b76028bb5de0d5773
Gareth Jones <garetrhsion@googlemail.com>
2019-10-18 09:58:07 +0100

    Removed Logging

--- SpinTools/qmech/qmech.py
+++ SpinTools/qmech/qmech.py
@@ -1,19 +1,13 @@
 import numpy as np
-import Logger
 
 class Qmech:
     def __init__(self):
-        log = Logger.Logger("../../logs/" + __name__)
-        self.__logger = log.get_logger()
-        self.__logger.info("Qmech() Constructor Called")
         return
 
     def angular_momentum(self,J):
         """ Find the angular momentum operator for a given total angular momentum J=L+S"""
-        self.__logger.info('angular_momentum({0}'.format(J))
 
         if type(J) not in [int,float]:
-            self.__logger.error("Incorrect type {0} passed to method".format(J))
             raise TypeError("J must be a non-negative, real, numerical value")
 
         alpha = lambda mz,J : (J*(J+1)-mz*(mz+1))**0.5
@@ -24,7 +18,6 @@ class Qmech:
         J_minus = np.zeros((dim,dim))
         Jz = np.zeros((dim,dim))
 
-        self.__logger.debug("# Calculate J+, J-, and Jz ")
         for n in range(dim):
             for m in range(dim):
                 if n==m+1:
@@ -34,19 +27,14 @@ class Qmech:
                 if n==m:
                     Jz[n,m] = J-n
 
-        self.__logger.debug("# Calculate Jx and Jy ")
         Jx = 0.5*(J_plus+J_minus)
         Jy = -0.5*1j*(J_plus-J_minus)
 
         dic = {'x':Jx, 'y':Jy, 'z':Jz }
-        self.__logger.debug("# Return dictionary of J components ")
         return dic
 
     def enlarge_matrix(self,M,J):
         """ Enlarge a matrix by taking the Kronecker product"""
-        self.__logger.info('enlarge_matrix({0},{1}'.format(M,J))
-
-        self.__logger.debug("# Return enlarged matrices ")
         return np.kron(np.identity(M),J)
     
     # def pauli_matrix(self,M=1,op='x'):
--- SpinTools/spinhamiltonian/spinhamiltonian.py
+++ SpinTools/spinhamiltonian/spinhamiltonian.py
@@ -1,22 +1,12 @@
 from scipy.constants import physical_constants as spc
 from SpinTools.qmech import qmech as qm
-# from SpinTools.logger import logger
-import Logger
-# import logging
 import numpy as np
 import json
 
 class SpinHamiltonian:
     def __init__(self,species):
         """ Constructor method """
-
-        # # Create and configure logger
-        log = Logger.Logger("../../logs/" + __name__)
-        self.__logger = log.get_logger()
-        self.__logger.info("SpinHamiltonian({0})".format(species))
-
         if type(species) not in [str]:
-            self.__logger.error("Incorrect type {0} passed to constructor".format(species))
             raise TypeError("Species must be a string")
 
         self.__ub = spc["Bohr magneton"][0]
@@ -25,9 +15,7 @@ class SpinHamiltonian:
         
         try:
            with open('../../species.json', 'r') as f:
-               self.__logger.debug("# Species JSON file opened")
                species_string = f.read()    
-               self.__logger.debug("# Species JSON file parsed")
            sp = json.loads(species_string)
         except FileNotFoundError:
            "The species json file is not present in the package directory"
@@ -42,51 +30,40 @@ class SpinHamiltonian:
     
     def electron_zeeman(self,B):
         """Calculate the electron Zeeman interaction"""
-        self.__logger.info('electron_zeeman({0}'.format(B))
 
         if type(B) not in [list]:
-            self.__logger.error("Incorrect type {0} passed to method".format(B))
             raise TypeError("B must be a vector")
         
         # if type(B.any()) not in [int,float]:
         #     raise TypeError("B components must be a float or integer value")
             
         S = self.__qm.angular_momentum(self.__ss['J'])
-        self.__logger.debug("# Enlarge spin matrices")
         Sx,Sy,Sz = (self.__qm.enlarge_matrix(self.__MI,S['x']),
                          self.__qm.enlarge_matrix(self.__MI,S['y']),
                          self.__qm.enlarge_matrix(self.__MI,S['z']))
 
-        self.__logger.debug("# return electron zeeman hamiltonian")
         return self.__ub*self.__ss['gS']*(Sx*B[0]+Sy*B[1]+Sz*B[2])
 
     def nuclear_zeeman(self,B):
         """Calculate the nuclear Zeeman interaction"""
-        self.__logger.info('nuclear_zeeman({0}'.format(B))
         if type(B) not in [list]:
-        	self.__logger.error("Incorrect type {0} passed to method".format(B))
         	raise TypeError("B must be a vector")
         
         # if type(B.any()) not in [int,float]:
         #     raise TypeError("B components must be a float or integer value")
         
         I = self.__qm.angular_momentum(self.__ss['I'])
-        self.__logger.debug("# Enlarge spin matrices")
         Ix,Iy,Iz = (self.__qm.enlarge_matrix(self.__MJ,I['x']),
                          self.__qm.enlarge_matrix(self.__MJ,I['y']),
                          self.__qm.enlarge_matrix(self.__MJ,I['z']))
-        self.__logger.debug("# return nuclear zeeman hamiltonian")
         return self.__un*self.__ss['gI']*((Ix*B[0]+Iy*B[1]+Iz*B[2]))
     
     def hyperfine(self):
         """Calculate the hyperfine interaction"""
-        self.__logger.info('hyperfine()')
         
-        self.__logger.debug("# Get electron and nuclear spin operators")
         S = self.__qm.angular_momentum(self.__ss['J'])
         I = self.__qm.angular_momentum(self.__ss['I'])
 
-        self.__logger.debug("# return hyperfine hamiltonian")
         return self.__h * self.__ss['A'] * (np.kron(S['x'],I['x']) + 
                 np.kron(S['y'],I['y']) + np.kron(S['z'],I['z']))
 
@@ -95,20 +72,16 @@ class SpinHamiltonian:
         self.__logger.info('get_hamiltonian({0}'.format(B))
 
         if type(B) not in [list]:
-        	self.__logger.error("Incorrect type {0} passed to method".format(B))
         	raise TypeError("B must be a vector")
         
         # if type(np.array(B).any()) not in [int,float]:
         #     raise TypeError("B components must be a float or integer value")
-        self.__logger.debug("# return full hamiltonian")
         return self.hyperfine() + self.electron_zeeman(B) + self.nuclear_zeeman(B)
 
     def get_field_vector(self,bx,by,bz):
         """Get field vector [bx, by, bz]"""     
-        self.__logger.info('get_field_vector({0},{1},{2})'.format(bx,by,bz))
 
         if (type(bx) not in [int,float]) or (type(by) not in [int,float]) or (type(bz) not in [int,float]):
-            self.__logger.error("Incorrect type passed to method")
             raise TypeError("B components must be a float or integer value")
         
         self.__logger.debug("# return field vector")
@@ -116,54 +89,39 @@ class SpinHamiltonian:
 
     def get_field_sweep(self,bmin=0,bmax=1,bnum=1000):
         """Sweep a field along one axis"""
-        self.__logger.info('get_field_sweep({0},{1},{2})'.format(bmin,bmax,bnum))
         if (type(bmin) not in [int,float]) or (type(bmax) not in [int,float]):
-        	self.__logger.error("Incorrect type passed to method")
         	raise TypeError("B components must be a float or integer value")
         if type(bnum) not in [int]:
-        	self.__logger.error("Incorrect type passed to method")
         	raise TypeError("B iterator must be an integer value")
             
-        self.__logger.debug("# return field sweep")
         return np.around(np.linspace(bmin,bmax,bnum),4) 
     
     def calculate_energy(self,Bz):
         """Calculate the eigenergy"""
-        self.__logger.info('calculate_energy({0})'.format(Bz))
 
         if type(Bz) not in [np.ndarray,int,float]:
-        	self.__logger.error("Incorrect type {0} passed to method".format(Bz))
         	raise TypeError("B components must be a float or integer value")
             
-        self.__logger.debug("# get Hamiltonian")
         H = [self.get_hamiltonian([0,0,Bz[i]]) for i in range(len(Bz))]
         
-        self.__logger.debug("# calculate eigenvalues")
         eigval = [np.linalg.eig(H[i])[0] for i in range(len(H))]
 
-        self.__logger.debug("# return sorted eigenvalues")
         return [np.sort(np.real(eigval[i]))/self.__h/1e09 for i in range(len(eigval))]
 
     def calculate_eigenvectors(self,Bz):
         """Calculate the eigenergy"""
-        self.__logger.info('calculate_eigenvectors({0})'.format(Bz))
 
         if type(Bz) not in [np.ndarray,int,float]:
-        	self.__logger.error("Incorrect type {0} passed to method".format(Bz))
         	raise TypeError("B components must be a float or integer value")
             
-        self.__logger.debug("# get Hamiltonian")
         H = [self.get_hamiltonian([0,0,Bz[i]]) for i in range(len(Bz))]
         
-        self.__logger.debug("# calculate eigenvectors")
         eigvec = [np.linalg.eig(H[i])[1] for i in range(len(H))]
 
-        self.__logger.debug("# return sorted eigenvectors")
         return eigvec
         # return [np.sort(np.real(eigvec[i]))/self.__h/1e09 for i in range(len(eigvec))]
 
     def FGR_transitions(self,Bz,B_drive=[1,0,0]):
-        self.__logger.info('FGR_transitions(Bz,{0})'.format(B_drive))
         # MI = M(species[0])
 
         # (Ix,Iy,Iz) = ang_mo_op(species[0])
@@ -174,14 +132,11 @@ class SpinHamiltonian:
 
         Es,gammas,nm,Efs,Eis,Ejs = [],[],[], [], [], []
 
-        self.__logger.debug('# Calculate eigenvectors')
         eigvec = self.calculate_eigenvectors(Bz)
 
-        self.__logger.debug('# Get perturbation hamiltonian')
         H_drive = self.electron_zeeman(B_drive)
         length = len(np.squeeze(np.asarray(eigvec[0][0,:])))
 
-        self.__logger.debug('# get gamma')
         for n in range(length):
             i = np.squeeze(np.asarray(eigvec[0][:,n]))
             for m in range(n+1,len(i)):
@@ -201,7 +156,6 @@ class SpinHamiltonian:
             #print(Ef,Ei)
 
         import pdb; pdb.set_trace()
-        self.__logger.debug('# Put Es in array')
         Es = (np.array(Es))
         # #print(Es[:,1].min())
         # self.__logger.debug('# Sort Es')
