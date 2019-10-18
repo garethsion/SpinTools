7e6ffbb1ec2d6d815106b4c7431fb87d48f36ba5
Gareth Jones <garetrhsion@googlemail.com>
2019-10-18 09:59:14 +0100

    Removed the .__ in the self variables

--- SpinTools/spinhamiltonian/spinhamiltonian.py
+++ SpinTools/spinhamiltonian/spinhamiltonian.py
@@ -9,9 +9,9 @@ class SpinHamiltonian:
         if type(species) not in [str]:
             raise TypeError("Species must be a string")
 
-        self.__ub = spc["Bohr magneton"][0]
-        self.__un = spc["nuclear magneton"][0]
-        self.__h = spc["Planck constant"][0]
+        self.ub = spc["Bohr magneton"][0]
+        self.un = spc["nuclear magneton"][0]
+        self.h = spc["Planck constant"][0]
         
         try:
            with open('../../species.json', 'r') as f:
@@ -19,12 +19,12 @@ class SpinHamiltonian:
            sp = json.loads(species_string)
         except FileNotFoundError:
            "The species json file is not present in the package directory"
-        self.__ss = sp[species]
+        self.ss = sp[species]
         
-        self.__MJ = int((self.__ss['J']*2)+1)
-        self.__MI = int((self.__ss['I']*2)+1)
+        self.MJ = int((self.ss['J']*2)+1)
+        self.MI = int((self.ss['I']*2)+1)
         
-        self.__qm = qm.Qmech()
+        self.qm = qm.Qmech()
         
         return
     
@@ -37,12 +37,12 @@ class SpinHamiltonian:
         # if type(B.any()) not in [int,float]:
         #     raise TypeError("B components must be a float or integer value")
             
-        S = self.__qm.angular_momentum(self.__ss['J'])
-        Sx,Sy,Sz = (self.__qm.enlarge_matrix(self.__MI,S['x']),
-                         self.__qm.enlarge_matrix(self.__MI,S['y']),
-                         self.__qm.enlarge_matrix(self.__MI,S['z']))
+        S = self.qm.angular_momentum(self.ss['J'])
+        Sx,Sy,Sz = (self.qm.enlarge_matrix(self.MI,S['x']),
+                         self.qm.enlarge_matrix(self.MI,S['y']),
+                         self.qm.enlarge_matrix(self.MI,S['z']))
 
-        return self.__ub*self.__ss['gS']*(Sx*B[0]+Sy*B[1]+Sz*B[2])
+        return self.ub*self.ss['gS']*(Sx*B[0]+Sy*B[1]+Sz*B[2])
 
     def nuclear_zeeman(self,B):
         """Calculate the nuclear Zeeman interaction"""
@@ -52,24 +52,24 @@ class SpinHamiltonian:
         # if type(B.any()) not in [int,float]:
         #     raise TypeError("B components must be a float or integer value")
         
-        I = self.__qm.angular_momentum(self.__ss['I'])
-        Ix,Iy,Iz = (self.__qm.enlarge_matrix(self.__MJ,I['x']),
-                         self.__qm.enlarge_matrix(self.__MJ,I['y']),
-                         self.__qm.enlarge_matrix(self.__MJ,I['z']))
-        return self.__un*self.__ss['gI']*((Ix*B[0]+Iy*B[1]+Iz*B[2]))
+        I = self.qm.angular_momentum(self.ss['I'])
+        Ix,Iy,Iz = (self.qm.enlarge_matrix(self.MJ,I['x']),
+                         self.qm.enlarge_matrix(self.MJ,I['y']),
+                         self.qm.enlarge_matrix(self.MJ,I['z']))
+        return self.un*self.ss['gI']*((Ix*B[0]+Iy*B[1]+Iz*B[2]))
     
     def hyperfine(self):
         """Calculate the hyperfine interaction"""
         
-        S = self.__qm.angular_momentum(self.__ss['J'])
-        I = self.__qm.angular_momentum(self.__ss['I'])
+        S = self.qm.angular_momentum(self.ss['J'])
+        I = self.qm.angular_momentum(self.ss['I'])
 
-        return self.__h * self.__ss['A'] * (np.kron(S['x'],I['x']) + 
+        return self.h * self.ss['A'] * (np.kron(S['x'],I['x']) + 
                 np.kron(S['y'],I['y']) + np.kron(S['z'],I['z']))
 
     def get_hamiltonian(self,B):
         """Get the full spin hamiltonian"""
-        self.__logger.info('get_hamiltonian({0}'.format(B))
+        self.logger.info('get_hamiltonian({0}'.format(B))
 
         if type(B) not in [list]:
         	raise TypeError("B must be a vector")
@@ -84,7 +84,7 @@ class SpinHamiltonian:
         if (type(bx) not in [int,float]) or (type(by) not in [int,float]) or (type(bz) not in [int,float]):
             raise TypeError("B components must be a float or integer value")
         
-        self.__logger.debug("# return field vector")
+        self.logger.debug("# return field vector")
         return [bx, by, bz]
 
     def get_field_sweep(self,bmin=0,bmax=1,bnum=1000):
@@ -106,7 +106,7 @@ class SpinHamiltonian:
         
         eigval = [np.linalg.eig(H[i])[0] for i in range(len(H))]
 
-        return [np.sort(np.real(eigval[i]))/self.__h/1e09 for i in range(len(eigval))]
+        return [np.sort(np.real(eigval[i]))/self.h/1e09 for i in range(len(eigval))]
 
     def calculate_eigenvectors(self,Bz):
         """Calculate the eigenergy"""
@@ -119,7 +119,7 @@ class SpinHamiltonian:
         eigvec = [np.linalg.eig(H[i])[1] for i in range(len(H))]
 
         return eigvec
-        # return [np.sort(np.real(eigvec[i]))/self.__h/1e09 for i in range(len(eigvec))]
+        # return [np.sort(np.real(eigvec[i]))/self.h/1e09 for i in range(len(eigvec))]
 
     def FGR_transitions(self,Bz,B_drive=[1,0,0]):
         # MI = M(species[0])
@@ -144,8 +144,8 @@ class SpinHamiltonian:
                 gamma = 1e24*np.abs(np.matmul(f,np.squeeze(np.asarray(np.matmul(H_drive,i)))))
 
                 if gamma > 0 :
-                    Ef = np.real(eigvec[1][m])/self.__h/1E9
-                    Ei = np.real(eigvec[1][n])/self.__h/1E9
+                    Ef = np.real(eigvec[1][m])/self.h/1E9
+                    Ei = np.real(eigvec[1][n])/self.h/1E9
                     E=(np.abs(Ef-Ei))
                     # Es.append([E,gamma,Ef,Ei])
                     Es.append([E])
@@ -158,9 +158,9 @@ class SpinHamiltonian:
         import pdb; pdb.set_trace()
         Es = (np.array(Es))
         # #print(Es[:,1].min())
-        # self.__logger.debug('# Sort Es')
+        # self.logger.debug('# Sort Es')
         # print(Es)
         # return Es
         # Es = Es[Es[:,0].argsort()]
-        # self.__logger.debug('# return Es')
+        # self.logger.debug('# return Es')
         # return(Es[:,0],Es[:,1],Es[:,2],Es[:,3]) 
